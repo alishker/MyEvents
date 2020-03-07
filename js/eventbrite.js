2b8b6c2f1b3f1238f8fa1b13b9678a3c9b5d0d1d
alishker <alinoorshukri7@gmail.com>
2020-03-07 22:11:04 +0300

    More commits

--- js/eventbrite.js
+++ js/eventbrite.js
@@ -1,23 +1,23 @@
-class EventBrite{
-    constructor(){
+class EventBrite {
+    constructor() {
         this.auth_token = 'KZXZUEVF5RWRVL3NNRN5';
         this.orderby = 'date';
     }
 
     async queryAPI(eventName, category) {
-        const eventsResponse = await fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${eventName}&sort_by=${this.orderby}&categories=${category}&token=${this.auth_token}`);
+        const eventsResponse = await fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${eventName}&sort_by=${this.orderby}&categories=${category}&token=KZXZUEVF5RWRVL3NNRN5`);
 
         // Wait for response and return as json
 
         const events = await eventsResponse.json();
 
         return {
-             events
+            events
         }
-   }
-    
-    async getcryptocurrencies(){
-        const categoriesResponse = await fetch(`https://www.eventbriteapi.com/v3/categories/?token=${this.auth_token}`)
+    }
+
+    async getcryptocurrencies() {
+        const categoriesResponse = await fetch(`https://www.eventbriteapi.com/v3/categories/?token=KZXZUEVF5RWRVL3NNRN5`)
         const categories = await categoriesResponse.json();
         return {
             categories
